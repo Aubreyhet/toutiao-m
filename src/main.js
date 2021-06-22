87006a0e15f7b42d9fb2e532a8d0a13744bb6f4b
aubreyhet <2930603106@qq.com>
2021-06-22 20:46:35 +0800

    目录结构处理

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+// import router from './router'
 import store from './store'
+
+//  导入全局样式
+import './styles/index.css'
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
+  // router,
   store,
   render: h => h(App)
 }).$mount('#app')
