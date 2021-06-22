3e14eda80c3dc96b28feb4ef7713c03ff3819c8e
aubreyhet <2930603106@qq.com>
2021-06-22 22:21:45 +0800

    vant的使用

--- src/main.js
+++ src/main.js
@@ -1,15 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
-// import router from './router'
+import router from './router'
 import store from './store'
+//  导入vant组件库
+import Vant from 'vant'
+//  引入组件库样式
+import 'vant/lib/index.css'
 
 //  导入全局样式
 import './styles/index.css'
+//  字体图标样式
+import './styles/icon.css'
+
+// 使用vant
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
 new Vue({
-  // router,
+  router,
   store,
   render: h => h(App)
 }).$mount('#app')
